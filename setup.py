0b4f3e66ec70cad320c09e114777718fadecd4c5
Brightcells <brightcells@gmail.com>
2018-06-27 17:44:25 +0800

    :tada: Initial

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+version = '1.0.0'
+
+setup(
+    name='django-mobi2',
+    version=version,
+    keywords='Django UserAgent',
+    description='Django middleware and view decorator to detect phones and small-screen devices',
+    long_description=open('README').read(),
+
+    url='https://github.com/django-xxx/django-mobi2.git',
+
+    author='Hackathon',
+    author_email='kimi.huang@brightcells.com',
+
+    packages=['mobi'],
+    py_modules=[],
+    package_data={
+        'mobi': ['*.txt']
+    },
+    install_requires=['django-six'],
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Office/Business :: Financial :: Spreadsheet',
+    ],
+)
